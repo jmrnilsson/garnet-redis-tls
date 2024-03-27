52e3ad1f3ee3ea315c5d7e517da8f2022fc48154
jmrnilsson <martin.jr.nilsson@gmail.com>
2024-03-27 21:14:53 +0100

    add jest

--- javascript/out/ioredisGarnetPlain.js
+++ javascript/out/ioredisGarnetPlain.js
@@ -8,8 +8,7 @@ const node_assert_1 = __importDefault(require("node:assert"));
 const ioredis_1 = __importDefault(require("ioredis"));
 const IOREDIS_OPTIONS = {
     host: 'localhost',
-    // port: 3278,
-    port: 7088,
+    port: 3278,
     maxRetriesPerRequest: 0,
 };
 async function ioredisSupportsGarnet() {
@@ -20,9 +19,19 @@ async function ioredisSupportsGarnet() {
     return result;
 }
 (0, node_test_1.describe)("ioredis (ts)", async () => {
-    await (0, node_test_1.test)("Garnet", async () => {
-        const response = await ioredisSupportsGarnet();
-        node_assert_1.default.equal(response, 'ioGarny');
+    let client = new ioredis_1.default(IOREDIS_OPTIONS);
+    (0, node_test_1.after)(() => {
+        client = undefined;
+    });
+    (0, node_test_1.test)("Garnet: set, get, getset", async () => {
+        const set = await client.set('sdASD', 'ioGarny');
+        const get = await client.get('sdASD');
+        const getset = await client.getset('sdASD', 'fin');
+        const get0 = await client.get('sdASD');
+        node_assert_1.default.equal(set, 'OK');
+        node_assert_1.default.equal(get, 'sdASD');
+        node_assert_1.default.equal(getset, 'sdASD');
+        node_assert_1.default.equal(get0, 'fin');
     });
 });
 // describe("ioredis (noop-ts)", async () => {
